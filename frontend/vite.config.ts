79e8cc4b643dd5520f645ce95b32878781c40211
Leon Grass <leon.grass@gmx.net>
2026-03-10 17:08:48 +0000

    changed docker slightly

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,5 +16,11 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+    allowedHosts: ['leongrass.ch', 'www.leongrass.ch'],
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['leongrass.ch', 'www.leongrass.ch', 'localhost'],
   },
 })
