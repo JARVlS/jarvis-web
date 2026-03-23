435bf588f1833b1df15e1ab86e91da57fd051b59
Leon Grass <leon.grass@gmx.net>
2026-03-23 13:39:58 +0000

    allow new URL host jarvis.leongrass.ch

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,11 +16,11 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-    allowedHosts: ['leongrass.ch', 'www.leongrass.ch'],
+    allowedHosts: ['leongrass.ch', 'www.leongrass.ch', 'jarvis.leongrass.ch'],
   },
   preview: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: ['leongrass.ch', 'www.leongrass.ch', 'localhost'],
+    allowedHosts: ['leongrass.ch', 'www.leongrass.ch', 'jarvis.leongrass.ch', 'localhost'],
   },
 })
